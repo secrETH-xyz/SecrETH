1b923f5ea630ac621e164ec51a39daa85832bf2b
ezzatisawesome <ezzatsuhaime@gmail.com>
2022-11-05 17:49:08 -0700

    Adding process.env support

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config({ path: __dirname + '/process.env' });
+
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_KEY;
+console.log(GOERLI_PRIVATE_KEY)
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://goerli.prylabs.net`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
