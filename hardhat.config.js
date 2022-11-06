ac0f17994d86d02ef6133c8363e6df8f48b8c418
ea-coding <92234415+ea-coding@users.noreply.github.com>
2022-11-05 18:31:25 -0700

    Fix bugs in JS demo

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config({ path: __dirname + '/process.env' });
+//require('dotenv').config({ path: __dirname + '/process.env' });
 require("@nomiclabs/hardhat-waffle");
 
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_KEY;
@@ -8,10 +8,10 @@ console.log(GOERLI_PRIVATE_KEY)
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  networks: {
-    goerli: {
-      url: `https://rpc.ankr.com/eth_goerli`,
-      accounts: [GOERLI_PRIVATE_KEY]
-    }
-  }
+  // networks: {
+  //   goerli: {
+  //     url: `https://rpc.ankr.com/eth_goerli`,
+  //     accounts: [GOERLI_PRIVATE_KEY]
+  //   }
+  // }
 };
